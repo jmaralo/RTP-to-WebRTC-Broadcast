8e1fbe013755e6ca88fce6a958f20e1ae63c7e63
jmaralo <martinezalonsojn@gmail.com>
2023-02-16 15:20:36 +0100

    add command line arguments

--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/jmaralo/rtp-to-webrtc-broadcast/broadcast"
 	"github.com/pion/webrtc/v3"
 )
 
+var streamURL = flag.String("i", "127.0.0.1:1234", "URL for the RTP stream")
+var trackID = flag.String("tid", "rtp", "The ID that identifies the video track")
+var streamID = flag.String("sid", "video", "The ID that identifies the video stream")
+var mtu = flag.Uint("mtu", 1500, "The MTU of the interface where the RTP stream is received")
+var config = flag.String("config", "./config.json", "Configuration file for the Peers")
+var signalURL = flag.String("o", "127.0.0.1:2345", "URL to signal the connection, the actual endpoint ws://<signal url>/signal")
+
 func main() {
-	broadcastHandle, err := broadcast.NewBroadcastHandle("192.168.1.3:6969", "video", "cam1", 1600, webrtc.Configuration{})
+	flag.Parse()
+
+	configRaw, err := os.ReadFile(*config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var conf = new(webrtc.Configuration)
+	if err := json.Unmarshal(configRaw, conf); err != nil {
+		log.Fatalln(err)
+	}
+
+	broadcastHandle, err := broadcast.NewBroadcastHandle(*streamURL, *streamID, *trackID, *mtu, webrtc.Configuration{})
 	if err != nil {
 		log.Println(err)
 	}
 
 	http.Handle("/signal", broadcastHandle)
-	//http.Handle("/", http.FileServer(http.Dir("./build")))
-	log.Println(http.ListenAndServe("192.168.1.3:4050", nil))
+	fmt.Printf("Listening for signals on ws://%s/signal\n", *signalURL)
+	log.Println(http.ListenAndServe(*signalURL, nil))
 }
